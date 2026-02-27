754e1d7794b8b6f38e73c2395390d5d0fb84e72e
julioOv-1104 <julioovalle202232237@cunoc.edu.gt>
2026-02-27 13:59:46 -0600

    creacion del proyecto java con sus dependencias para usar Mysql

---
+++ Practica1_IPC2_pizzaTycoon/src/main/java/paquete/practica1_ipc2_pizzatycoon/Practica1_IPC2_pizzaTycoon.java
@@ -0,0 +1,9 @@
+
+package paquete.practica1_ipc2_pizzatycoon;
+
+public class Practica1_IPC2_pizzaTycoon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
